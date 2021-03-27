14a904f7291cbf104d9c7e1bcc17198ca44a19cb
slavo3dev <slavo@slavo3.com>
2021-03-27 15:17:20 -0400

    adding new components

---
+++ components/eventDetail/index.js
@@ -0,0 +1,6 @@
+import { EventContent } from "./eventContent";
+import { EventLogistics } from "./eventLogistics";
+import { EventSummary } from "./eventSummary";
+import { LogisticsItem } from "./logisticsItem";
+
+export { EventContent, EventLogistics, EventSummary, LogisticsItem };
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,15 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+
 function event() {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const envet = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found</p>;
+  }
   return (
     <div>
       <h1>Event Page</h1>
